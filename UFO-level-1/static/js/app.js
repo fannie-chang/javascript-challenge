798ea062483e16450ddd5817c6f04b063139f1ce
Fannie Chang <fanniech@gmail.com>
2021-06-22 00:39:00 -0700

    update on UFO-level -1

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,66 @@
+
+
+// Get a reference to the table body
+
+var tbody = d3.select("tbody");
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+
+// Complete the event handler function for the form
+function runEnter() {
+
+	//Prevent the page from refreshing
+	d3.event.preventDefault();
+
+	//Select the input element and get the raw HTML node
+	var inputElement = d3.select("#datetime");
+
+	//Get the value property of the input element
+	var inputValue = inputElement.property("value");
+
+//console.log(inputValue);
+//console.log(data);
+
+		
+	var filteredData = data.filter(data => data.datetime === inputValue);
+
+	filteredData.forEach(function(UFOSightings)  {
+
+	var row = tbody.append("tr");
+
+	Object.entries(UFOSightings).forEach(function([key,value])  {
+		console.log(key,value);
+
+		
+		var cell =row.append("td");
+				
+		cell.text(value);
+
+
+	});
+
+	});
+
+}
+
+
+	
+
+	
+
+
+	
+
+
+
+
+
